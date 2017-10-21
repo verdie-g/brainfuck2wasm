c764e3f566f9e9e886aafbb0a70f097cfa712ec8
verdie-g <gregoire.verdier@gmail.com>
2017-10-21 18:06:50 +0200

    Remove logs

--- worker.js
+++ worker.js
@@ -1,7 +1,6 @@
 importScripts('bf2wasm.js');
 
 onmessage = e => {
-    console.log('worker: ', e.data);
     const bf = e.data;
     const wasm = bfToWasm(bf);
     const memory = new WebAssembly.Memory({ initial: 2 });
