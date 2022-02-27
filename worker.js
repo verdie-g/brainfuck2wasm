32ba63b3ec526c1c9796fb2a745040fcb4b782a7
verdie-g <gregoire.verdier@gmail.com>
2022-02-27 16:27:03 +0100

    Fix compilation

--- worker.js
+++ worker.js
@@ -5,5 +5,7 @@ onmessage = e => {
     const wasm = bfToWasm(bf);
     const memory = new WebAssembly.Memory({ initial: 2 });
     const imports = { imports: { memory: memory } };
-    WebAssembly.instantiate(wasm, imports).then(instance => postMessage(memory.buffer));
+    WebAssembly.instantiate(wasm, imports).then((instance) =>
+        postMessage(new Int32Array(memory.buffer.slice(0, 65536)))
+    );
 };
